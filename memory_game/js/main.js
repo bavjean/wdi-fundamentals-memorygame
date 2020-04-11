3273cbb98ea412c22f8e1f617db4e04359444143
Brian Avjean <brian@Brians-MBP.fios-router.home>
2020-04-11 19:05:07 -0400

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
-let cardOne = "queen";
-let cardTwo = "king";
-let cardThree = "queen";
-let cardFour = "king";
+const cards = ["queen", "queen", "king", "king"];
+const cardsInPlay = [];
+let cardOne = cards[0];
+let cardTwo = cards[1];
 
-console.log("User flipped " + cardOne)
-console.log("User flipped " + cardTwo)
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+
+console.log("User flipped " + cardsInPlay[0]);
+console.log("User flipped " + cardsInPlay[1]);
+
+if (cardsInPlay.length === 2) {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		alert("You found a match!");
+	} else {
+		alert("Sorry, try again.");
+	}
+}
